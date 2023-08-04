f4e1c541e1f18b73f82be3a468d95953062a85d7
pramila patel <pramilapatel095@gmail.com>
2023-08-04 09:00:48 +0530

    footer

--- include/header.php
+++ include/header.php
@@ -5,19 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>website</title>
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <link rel="stylesheet" href="css/footer.css">
-=======
-<<<<<<< HEAD
     <link rel="stylesheet" href="css/style.css">
-=======
-    <link rel="stylesheet" href="service.css">
->>>>>>> 645d2aa3a3a781f3c7032595aeb594a3ba7b02fd
->>>>>>> eeadf87e525c2e207df7704139716fcdae723041
-=======
-    <link rel="stylesheet" href="css/style.css">
->>>>>>> f6272d23777ae26bc7f587a9d47aa9ab9d96c1ba
 </head>
 <body>
     <div class="header">
@@ -29,7 +17,6 @@
             <li><a href="gallery">Gallery</a></li>
         </ul>
     </div>
-<<<<<<< HEAD
 
     <div class="footer">
         <div class="para">
@@ -45,5 +32,3 @@
 
 
  
-=======
->>>>>>> eeadf87e525c2e207df7704139716fcdae723041
