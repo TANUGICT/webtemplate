377501fe701217a994e2634e1605c98a46b75c84
TANUGICT <tanugict@gmail.com>
2023-08-04 08:19:49 +0530

    Service.php

---
+++ service.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Service page</title>
+    <link rel="stylesheet" href="service.css">
+</head>
+<body>
+    <h1>Service Page</h1>
+    <div class="service">
+        <p>Web devilopment & App created</p>
+    </div>
+    <div class="service" style="background-color:red;">
+        <p>Web devilopment & App created</p>
+    </div>
+    <div class="service" style="background-color:pink;">
+        <p>Web devilopment & App created</p>
+    </div>
+    <div class="service" style="background-color:green;">
+        <p>Web devilopment & App created</p>
+    </div>
+    <div class="service" style="background-color:yellow;">
+        <p>Web devilopment & App created</p>
+    </div>
+</body>
+</html>
