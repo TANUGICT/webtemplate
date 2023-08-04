4161707baf228d4bb99ead1c6db7480d03cb51b6
saritanisha <saritapa291@gmail.com>
2023-08-04 09:04:06 +0530

    home.php

--- home.php
+++ home.php
@@ -1,3 +1,5 @@
 <?php include 'include/header.php'?>
+
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum est necessitatibus odit, itaque fugiat adipisci ipsa sit voluptates harum aliquid veritatis incidunt, magni, maiores non iste magnam natus eius obcaecati.</p>
+
 <?php include 'include/footer.php'?>
