859268c654bf9d251b078d925d2a18818ab48c14
rupma <rupmamaurya25@gmail.com>
2023-08-09 09:15:19 +0530

    contact.php

--- include/main.php
+++ include/main.php
@@ -5,22 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>website</title>
-    <link rel="stylesheet" href="css/service.css">
-    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/footer.css">
-    <link rel="stylesheet" href="css/contact.css">
-    <link rel="stylesheet" href="css/gallery.css">
-
 </head>
 <body>
     <div class="header">
         <ul>
-            <li><a href="main.php">Home</a></li>
+            <li><a href="home">Home</a></li>
             <li><a href="about">About</a></li>
-            <li><a href="Contact.php">Contact Us</a></li>
-            <li><a href="service.php">Services</a></li>
+            <li><a href="Contact">Contact Us</a></li>
+            <li><a href="services">Services</a></li>
             <li><a href="gallery">Gallery</a></li>
         </ul>
     </div>
 
+    <div class="footer">
+        <div class="para">
+        <h1>GICT COMPUTER</h1>
+        </div>
+        <div class="pa">
+        <h3>gictcomputer@gmail.com</h3>
+        </div>
+    </div>
+</body>
+</html>
 
+
+
+ 
