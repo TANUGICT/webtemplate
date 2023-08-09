210be632588017b7972fa8beb8b5b3e4a51fba61
pramila patel <pramilapatel095@gmail.com>
2023-08-09 08:17:20 +0530

    header

--- include/header.php
+++ include/header.php
@@ -13,7 +13,7 @@
             <li><a href="home">Home</a></li>
             <li><a href="about">About</a></li>
             <li><a href="Contact us">Contact Us</a></li>
-            <li><a href="services">Services</a></li>
+            <li><a href="service.php">Services</a></li>
             <li><a href="gallery">Gallery</a></li>
         </ul>
     </div>
